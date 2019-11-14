704674329e95e7f69dbd1a186ad8f2ae1a06610f
neerajp99 <neerajp1999@gmail.com>
2019-11-15 01:46:13 +0530

    Add react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    Standard Alpha
+      <Router>
+        <Route exact path="/" render={() => <Navbar />} />
+      </Router>
     </div>
   );
 }
