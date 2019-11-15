9db742a449fa6d6f189a3861fe50163aa89c644e
neerajp99 <neerajp1999@gmail.com>
2019-11-15 17:27:08 +0530

    Modified Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,12 +3,12 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar navbar-expand-lg navbar-dark standard_navbar">
-      <a class="navbar-brand standard_heading" href="#">
+      <a className="navbar-brand standard_heading" href="#">
         {" "}
         Standard Alpha
       </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarTogglerDemo01"
@@ -16,14 +16,14 @@ class Navbar extends Component {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon" />
+          <span className="navbar-toggler-icon" />
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <form class="form-inline my-2 my-lg-0 standard_form">
-            <button class="btn btn-outline-success my-2 my-sm-0 standard_register_button">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <form className="form-inline my-2 my-lg-0 standard_form">
+            <button className="btn btn-outline-success my-2 my-sm-0 standard_register_button">
               Register
             </button>
-            <button class="btn btn-outline-success my-2 my-sm-0 standard_login_button">
+            <button className="btn btn-outline-success my-2 my-sm-0 standard_login_button">
               Login
             </button>
           </form>
