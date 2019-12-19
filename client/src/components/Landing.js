59bc6b41b9b5d548562ac1c361febf3e4382e49b
neerajp99 <neerajp1999@gmail.com>
2019-12-19 10:33:08 +0530

    Add tab pills

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import tickers from "../utils/symbols.json";
 import escapeRegExp from "escape-string-regexp";
+import StickyHeadTable from './tables/Tables'
 
 class Landing extends Component {
   state = {
     ticker: ""
   };
+
+  // If a user search something, change the query state
   onChange = event => {
     event.preventDefault();
     this.setState({
@@ -14,17 +17,18 @@ class Landing extends Component {
   };
   showlingTickers = () => {};
   render() {
+    // Check if the user asks for a query, and filter them out, else tickers
     const { ticker } = this.state;
     let showingTickers;
     if (ticker) {
       const match = new RegExp(escapeRegExp(ticker), "i");
       showingTickers = tickers.filter(tick => match.test(tick["SYMBOL"]));
-    } else{
-      showingTickers = tickers
+    } else {
+      showingTickers = tickers;
     }
     return (
       <div className="wrapper">
-        <div className="col-md-3 landing_left">
+        <div className="col-md-2 landing_left">
           <form className="form-inline my-2 my-lg-0">
             <input
               className="form-control mr-sm-2 ticker_input"
@@ -68,7 +72,99 @@ class Landing extends Component {
             })}
           </ul>
         </div>
-        <div className="col-md-9 landing_right">WOeld</div>
+        <div className="col-md-10 landing_right">
+          <div className="landing_right_top_main" >
+          <nav>
+            <div className="nav nav-tabs nav-fill full-nav-item" id="nav-tab" role="tablist">
+              <a
+                className="nav-item nav-link active nav_item_text"
+                id="nav-home-tab"
+                data-toggle="tab"
+                href="#chains"
+                role="tab"
+                aria-controls="nav-home"
+                aria-selected="true"
+              >
+                Chain & Greeks
+              </a>
+
+              <a
+                className="nav-item nav-link nav_item_text"
+                id="nav-contact-tab"
+                data-toggle="tab"
+                href="#spreads"
+                role="tab"
+                aria-controls="nav-contact"
+                aria-selected="false"
+              >
+                Spreads
+              </a>
+              <a
+                className="nav-item nav-link nav_item_text"
+                id="nav-contact-tab"
+                data-toggle="tab"
+                href="#lab"
+                role="tab"
+                aria-controls="nav-contact"
+                aria-selected="false"
+              >
+                Lab
+              </a>
+              <a
+                className="nav-item nav-link nav_item_text"
+                id="nav-contact-tab"
+                data-toggle="tab"
+                href="#test"
+                role="tab"
+                aria-controls="nav-contact"
+                aria-selected="false"
+              >
+                Test
+              </a>
+            </div>
+          </nav>
+
+          <div className="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+            <div
+              className="tab-pane fade show active mafia_main_content"
+              id="chains"
+              role="tabpanel"
+              aria-labelledby="nav-home-tab"
+            >
+            <StickyHeadTable/>
+            </div>
+
+            <div
+              className="tab-pane fade mafia_main_content"
+              id="spreads"
+              role="tabpanel"
+              aria-labelledby="nav-contact-tab"
+            >
+              oaooaoa
+            </div>
+            <div
+              className="tab-pane fade mafia_main_content"
+              id="lab"
+              role="tabpanel"
+              aria-labelledby="nav-contact-tab"
+            >
+              koishore
+            </div>
+            <div
+              className="tab-pane fade mafia_main_content"
+              id="test"
+              role="tabpanel"
+              aria-labelledby="nav-contact-tab"
+            >
+              mitaals
+            </div>
+          </div>
+          </div>
+          <div className="landing_right_bottom_main">
+            lalala
+          </div>
+
+        </div>
       </div>
     );
   }
