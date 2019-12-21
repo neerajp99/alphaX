da6b97c7d216aed7f7cee77dfa0cdd34bb5f55e8
neerajp99 <neerajp1999@gmail.com>
2019-12-21 21:12:51 +0530

    Add graph data reducer

---
+++ client/src/reducers/graphReducer.js
@@ -0,0 +1,17 @@
+import { GET_ERRORS, GET_GRAPH_DATA } from "../actions/graphsAction";
+
+const initialState = {
+  graphValues: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_GRAPH_DATA:
+      return {
+        ...state,
+        graphValues: action.payload
+      };
+    default:
+      return state;
+  }
+}
