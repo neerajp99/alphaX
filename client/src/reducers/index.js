c82a64ca86518ffc5d54b94585c348ed032a5569
neerajp99 <neerajp1999@gmail.com>
2019-12-22 00:04:55 +0530

    Edit Root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import valuesReducer from "./valuesReducer";
+import graphReducer from "./graphReducer"
 
 export default combineReducers({
-  values: valuesReducer
+  values: valuesReducer,
+  graphValues: graphReducer
 });
