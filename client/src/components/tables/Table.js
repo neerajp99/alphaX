56c6b69884df133db7f3c7f66744e919ddcc7a65
neerajp99 <neerajp1999@gmail.com>
2019-12-21 13:15:56 +0530

    Add dynamic function for positions table

--- client/src/components/tables/Table.js
+++ client/src/components/tables/Table.js
@@ -8,6 +8,8 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
+import { getValues } from "../../actions/valuesAction";
+import { useDispatch, useSelector } from "react-redux";
 
 const columns = [
   {
@@ -62,14 +64,6 @@ function createData(ticker, opt_type, strike, expiry, price, qty) {
   return { ticker, opt_type, strike, expiry, price, qty };
 }
 
-const rows = [
-  createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2),
-  createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2),
-  createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2),
-  createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2),
-  createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2)
-];
-
 const useStyles = makeStyles({
   root: {
     width: "100%"
@@ -92,6 +86,61 @@ export default function StickyHeadTable() {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+
+  // const options
+  const optionsState = React.useState([]);
+
+  // Get state values
+  const values = useSelector(state => state.values);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    console.log("VALUES", values.values);
+    // optionsState.push(values)
+    // console.log("VALUES State", optionsState)
+  });
+
+  // Table values here
+  const rowss = [
+    createData("values", "PE", 102, "10/06/19", 4, -2),
+    createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2),
+    createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2),
+    createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2),
+    createData("BANKIFTY", "PE", 102, "10/06/19", 4, -2)
+  ];
+
+  const rows = [];
+  if (values.values.length > 0) {
+    values.values[2].map(data => {
+      const ticker = "BANKNIFTY";
+      let opt_type = "";
+      if (data[1] === "call_ask") {
+        opt_type = "CE";
+      }
+      if (data[1] === "call_bid") {
+        opt_type = "CE";
+      }
+      if (data[1] === "puts_ask") {
+        opt_type = "PE";
+      }
+      if (data[1] === "puts_bid") {
+        opt_type = "PE";
+      }
+
+      const strike = data[0].strike;
+      const expiry = data[0].expiry;
+      const price = 1;
+      const qty = 2;
+
+      rows.push({
+        ticker,
+        opt_type,
+        strike,
+        expiry,
+        price,
+        qty
+      });
+    });
+  }
 
   return (
     <Paper className={classes.root}>
